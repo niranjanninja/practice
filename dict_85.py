273c9f10c9fc577d1f85357ba8a4f26cc0a5e8e0
Niranjan <niranjansabari56@gmail.com>
2025-04-28 01:46:54 +0530

    new file added

---
+++ dict_85.py
@@ -0,0 +1,11 @@
+grades = {"Alice": 85, "Bob": 92, "Charlie": 85, "Diana": 90}
+# Task: Write code to find all students who got the score 85.
+# Output: ["Alice", "Charlie"]
+
+a=[]
+for name,mark in grades.items():
+    if mark == 85:
+        a.append(name)
+print(a)
+
+
