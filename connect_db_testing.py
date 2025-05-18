253ea45f939a9a7995eb3cff485304ae905ab394
niranjanninja <niranjanrox56@gmail.com>
2025-05-18 20:14:05 +0530

    commit

---
+++ connect_db_testing.py
@@ -0,0 +1,300 @@
+import psycopg2
+import re
+import smtplib
+
+class Sql_database:
+    def connect_to_db(self):
+        # This method returns the connection object
+        ninja = psycopg2.connect(database="Customers",user="niranjansabari56",password="ninja2501",host="127.0.0.1",port=5432)
+        return ninja
+    
+    def check_db_connection(self):
+        connection_obj = self.connect_to_db()
+        if connection_obj:
+            # print("Connected to db successfully")
+            return connection_obj
+        else:
+            # print("Failed")
+            return False
+
+        def print_user_details(self):
+            connection_obj = self.check_db_connection()
+            if not connection_obj:
+                # print("Printing user details Failed")
+                return False
+
+            curr = connection_obj.cursor()
+            print(curr)
+            curr.execute("SELECT * FROM user_details;")
+            data = curr.fetchall()
+
+            # PRINT THE RECORDS
+            for row in data:
+                print(row)
+                print(data)
+    # print_user_details()
+
+    def get_user_detail(self):
+        user_name=str(input("Enter user name : "))
+        password=str(input("Enter password : "))
+        phone_number=str(input("Enter phone number : "))
+        mail_id=str(input("Enter mail id : "))
+        return [user_name,password,phone_number,mail_id]
+
+    def insert_details(self):
+        connection_obj=self.check_db_connection()
+        values=get_user_detail()
+        user_name=values[0]
+        password=values[1]
+        phone_number=values[2]
+        mail_id=values[3]
+        curr=connection_obj.cursor()
+        insert =f"INSERT INTO user_details (user_name,user_password,phn_number,mail_id) VALUES ('{user_name}','{password}','{phone_number}','{mail_id}')"
+        curr.execute(insert)
+        connection_obj.commit()
+        print(insert)
+    # insert_details()
+
+    def login_details(self):
+        connection_obj=self.check_db_connection()
+        username=str(input("Enter user name : "))
+        password=str(input("Enter password : "))
+        curr=connection_obj.cursor()
+        details=f"SELECT * FROM user_details WHERE user_name= '{username}' AND user_password= '{password}'"
+        curr.execute(details)
+        fetch=curr.fetchone()
+        print(fetch)
+        if fetch:
+            print("Login successful")
+        else:
+            print("Enter correct details")
+        # fetch=curr.fetchall()
+        # for value in fetch:
+        #     if username and password in value:
+        #         print("Login success")
+        #         return
+        # else:
+        #     print("Enter correct details")
+    # login_details()
+
+    def reset_password(self):
+        connection_obj=self.check_db_connection()
+        username=str(input("Enter user name : "))
+        mail=str(input("Enter mail id : "))
+        curr=connection_obj.cursor()
+        details=f"SELECT * FROM users WHERE user_name='{username}' AND mail_id='{mail}'"
+        curr.execute(details)
+        fetch=curr.fetchone()
+        if fetch:
+            new_pass=str(input("Enter new password : "))
+            confirm_pass=str(input("Confirm new password : "))
+            if new_pass==confirm_pass:
+                npass=f"UPDATE users SET user_pass = '{new_pass}' WHERE mail_id = '{mail}'"
+                curr.execute(npass)
+                connection_obj.commit()
+                print("Password changed successfully")
+            else:
+                print("Password does not match")
+        else:
+            print("Enter correct details")
+        # fetch=curr.fetchall()
+        # for value in fetch:
+        #     if username and mail in value:
+        #         new_pass=str(input("Enter new password : "))
+        #         confirm_pass=str(input("Confirm new password : "))
+        #         if new_pass==confirm_pass:
+        #             npass=f"UPDATE user_details SET user_password = '{new_pass}' WHERE mail_id = '{mail}'"
+        #             curr.execute(npass)
+        #             connection_obj.commit()
+        #             print("Password changed successfully")
+        #         else:
+        #             print("Password does not match")
+        #             return
+        #         return
+        # else:
+        #     print("Enter correct details")
+    # reset_password()
+
+    def update_user_details(self):
+        connection_obj=self.check_db_connection()
+        username=str(input("Enter user name : "))
+        password=str(input("Enter password : "))
+        curr=connection_obj.cursor()
+        details=f"SELECT * FROM user_details WHERE user_name='{username}' AND user_password='{password}'"
+        curr.execute(details)
+        fetch=curr.fetchone()
+        if fetch:
+            print("Login successful")
+            new_detail=str(input("Which detail has to be updated  \nPress 1 for username\nPress 2 for password\nPress 3 for phone number\nPress 4 for mail id\n:"))
+            if new_detail == "1":
+                update_name=str(input("Enter new user name : "))
+                updated_name=f"UPDATE user_details SET user_name = '{update_name}' WHERE user_password = '{password}'"
+                curr.execute(updated_name)
+                connection_obj.commit()
+                print("User name updated")
+                return
+            if new_detail == "2":
+                update_password=str(input("Enter new password : "))
+                updated_password=f"UPDATE user_details SET user_password = '{update_password}' WHERE user_name = '{username}'"
+                curr.execute(updated_password)
+                connection_obj.commit()
+                print("Password updated")
+                return
+            if new_detail == "3":
+                update_phone_number=str(input("Enter new phone number : "))
+                updated_phone_number=f"UPDATE user_details SET phn_number = '{update_phone_number}' WHERE user_name = '{username}'"
+                curr.execute(updated_phone_number)
+                connection_obj.commit()
+                print("Phone number updated")
+                return
+            if new_detail == "4":
+                update_mail_id=str(input("Enter new mail id : "))
+                updated_mail_id=f"UPDATE user_details SET mail_id = '{update_mail_id}' WHERE user_name = '{username}'"
+                curr.execute(updated_mail_id)
+                connection_obj.commit()
+                print("Mail id updated")
+                return
+            return
+        else:
+            print("Enter correct details")
+
+        # for values in fetch:
+        #     if username and password in values:
+        #         print("Login successful")
+        #         new_detail=str(input("Which detail has to be updated  \nPress 1 for username\nPress 2 for password\nPress 3 for phone number\nPress 4 for mail id\n:"))
+        #         if new_detail == "1":
+        #             update_name=str(input("Enter new user name : "))
+        #             updated_name=f"UPDATE user_details SET user_name = '{update_name}' WHERE user_password = '{password}'"
+        #             curr.execute(updated_name)
+        #             connection_obj.commit()
+        #             print("User name updated")
+        #             return
+        #         if new_detail == "2":
+        #             update_password=str(input("Enter new password : "))
+        #             updated_password=f"UPDATE user_details SET user_password = '{update_password}' WHERE user_name = '{username}'"
+        #             curr.execute(updated_password)
+        #             connection_obj.commit()
+        #             print("Password updated")
+        #             return
+        #         if new_detail == "3":
+        #             update_phone_number=str(input("Enter new phone number : "))
+        #             updated_phone_number=f"UPDATE user_details SET phn_number = '{update_phone_number}' WHERE user_name = '{username}'"
+        #             curr.execute(updated_phone_number)
+        #             connection_obj.commit()
+        #             print("Phone number updated")
+        #             return
+        #         if new_detail == "4":
+        #             update_mail_id=str(input("Enter new mail id : "))
+        #             updated_mail_id=f"UPDATE user_details SET mail_id = '{update_mail_id}' WHERE user_name = '{username}'"
+        #             curr.execute(updated_mail_id)
+        #             connection_obj.commit()
+        #             print("Mail id updated")
+        #             return
+        #         return
+        # else:
+        #     print("Enter correct details")
+    # # update_user_details()
+    
+    def sign_up_page(self):
+        
+        connection_obj=self.check_db_connection()
+        curr=connection_obj.cursor()
+        def sign_up_user_name():
+            try:
+                global username
+                username=str(input("Enter user name : "))
+                u_name=f"SELECT * FROM sign_up_page WHERE user_name = '{username}'"
+                curr.execute(u_name)
+                fetch_name=curr.fetchone()
+                if fetch_name:
+                    print("User name already exist")
+                    return sign_up_user_name()
+            except Exception:
+                print("Something went wrong")
+        sign_up_user_name()
+
+
+        def sign_up_password():
+            try:
+                global password
+                pass_pattern = r'\w{10,100}$'
+                print("Password should have at least 10 characters Can be  mix of alphabets and numbers")
+                password=str(input("Enter password : "))
+                confirm_pass=str(input("Confirm  password again : "))
+                if password==confirm_pass:
+                    if re.search(pass_pattern,password):
+                        print("Valid password")
+                    else:
+                        print("Enter a valid password")
+                        return sign_up_password()
+                else:
+                    print("Password does not match")
+                    return sign_up_password()
+            except Exception:
+                print("Something went wrong")
+        sign_up_password()
+
+
+        def sign_up_phone_number():
+            try:
+                global phone_number
+                phn_pattern =r'^\d{10}$'
+                phone_number=str(input("Enter phone number : "))
+                if re.search(phn_pattern,phone_number):
+                    print("Valid number")
+                    num=f"SELECT * FROM sign_up_page WHERE phone_number = '{phone_number}'"
+                    curr.execute(num)
+                    fetch_num=curr.fetchone()
+                    if fetch_num:
+                        print("Phone number already exist")
+                        return sign_up_phone_number()
+                else:
+                    print("Enter a valid number")
+                    return sign_up_phone_number()
+            except Exception:
+                print("Something went wrong")  
+        sign_up_phone_number()
+
+
+        def sign_up_mail_id():
+            try:
+                global mail_id
+                mail_pattern =r'^[a-z A-Z 0-9]+[\._]?[a-z A-Z 0-9]+[@]\w+[.]\D{2,3}$'
+                mail_id=str(input("Enter mail id : "))
+                if re.search(mail_pattern,mail_id):
+                    print("Valid mail id")
+                    mail=f"SELECT * from sign_up_page WHERE mail_id = '{mail_id}'"
+                    curr.execute(mail)
+                    fetch_mail=curr.fetchone()
+                    if fetch_mail:
+                        print("Mail id already exist")
+                        return sign_up_mail_id() 
+                else:
+                    print("Enter a valid mail id")
+                    return sign_up_mail_id()
+                
+                sender="niranjanrox56@gmail.com"
+                receiver=(mail_id)
+                message=f"Hello {username}\n Welcome to our website"
+                server=smtplib.SMTP("smtp.gmail.com",587)
+                server.starttls()
+                server.login(sender,'jaxa cqze rzza hvyj')
+                server.sendmail(sender,receiver,message)
+                print(f"Email has been sent to {mail_id}")
+            except Exception:
+                print("Something went wrong")  
+        sign_up_mail_id()
+
+        def insert_value():
+            try:
+                insert=f"INSERT INTO sign_up_page (user_name,user_pass,phone_number,mail_id) VALUES ('{username}','{password}','{phone_number}','{mail_id}')"
+                curr.execute(insert)
+                connection_obj.commit()
+                print("Inserted into DB")
+            except Exception as e:
+                print(f"Something went wrong -> {e}")
+        insert_value()
+
+database=Sql_database()
+database.sign_up_page()
+
